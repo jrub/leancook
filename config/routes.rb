09ca02be689083007f19dc9bc29bff72ca5ee812
Rafa García <contacto@rafagarcia.net>
2014-10-18 12:37:47 +0200

    Add home page with some styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/index', as: :home
+  get 'home/search', as: :search
+
+  root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
