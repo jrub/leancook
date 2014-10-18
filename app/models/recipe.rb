a2c41c2f15b886ed7080baf40712be7c0704c761
Rafa García <contacto@rafagarcia.net>
2014-10-18 23:12:45 +0200

    Define search_by_ingredients method
    Used to search mainly by ingredients

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,10 @@
 class Recipe < ActiveRecord::Base
   searchkick
+
+  def self.search_by_ingredients(query)
+    search(
+      query,
+      fields: [:name, 'ingredients^10', :description]
+    )
+  end
 end
